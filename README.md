f859c0030b0a4f55182b6fee7ae7e8e88b84eecc
fakhrichaerul <fakhrichaerul@gmail.com>
2021-03-23 16:55:51 +0700

    Commit pertama

---
+++ README.md
@@ -0,0 +1,3 @@
+# Belajar Spring Core dengan @Annotation dan Java based Configuration
+
+Ini adalah Project Belajar Spring Core dengan @annotation dan Java based configuration
---
+++ src/main/java/com/fakhri/service/implementation/AccountServiceImpl.java
@@ -0,0 +1,41 @@
+package com.fakhri.service.implementation;
+
+import com.fakhri.domain.Account;
+import com.fakhri.repository.AccountRepository;
+import com.fakhri.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service(value = "accountService")
+public class AccountServiceImpl implements AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public void transferMoney(Long sourceId, Long destinationId, BigDecimal balance) {
+        Account sourceAccount = accountRepository.find(sourceId);
+        Account destinationAccount = accountRepository.find(destinationId);
+        sourceAccount.setBalance(sourceAccount.getBalance().subtract(balance));
+        destinationAccount.setBalance(destinationAccount.getBalance().add(balance));
+
+        accountRepository.update(sourceAccount);
+        accountRepository.update(destinationAccount);
+    }
+
+    @Override
+    public Account getAccount(Long id) {
+
+        return accountRepository.find(id);
+    }
+
+    public AccountRepository getAccountRepository() {
+        return accountRepository;
+    }
+
+    @Autowired
+    public void setAccountRepository(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+}
